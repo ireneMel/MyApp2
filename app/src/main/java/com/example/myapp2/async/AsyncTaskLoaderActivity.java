e21d1acc78410940d4f9f8a6c500e33612a933c1
ireneMiller <iryna2.melnyk@ukma.edu.com>
2022-02-20 19:53:49 +0200

    edited async task

--- app/src/main/java/com/example/myapp2/async/AsyncTaskLoaderActivity.java
+++ app/src/main/java/com/example/myapp2/async/AsyncTaskLoaderActivity.java
@@ -40,6 +40,7 @@ public class AsyncTaskLoaderActivity extends BaseActivity implements LoaderManag
         binding = ActivityAsyncTaskLoaderBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
 
+        binding.progressBar.setVisibility(View.GONE);
         if (getSupportLoaderManager().getLoader(0) != null)
             getSupportLoaderManager().initLoader(0, null, this);
     }
@@ -62,10 +63,11 @@ public class AsyncTaskLoaderActivity extends BaseActivity implements LoaderManag
             Bundle queryBundle = new Bundle();
             queryBundle.putString("query", query);
             getSupportLoaderManager().restartLoader(0, queryBundle, this);
-
+            binding.progressBar.setVisibility(View.VISIBLE);
             binding.authorText.setText("");
             binding.titleText.setText("loading");
         } else {
+            binding.progressBar.setVisibility(View.GONE);
             if (query.length() == 0) {
                 binding.authorText.setText("");
                 binding.titleText.setText("Your query has no results");
@@ -89,52 +91,41 @@ public class AsyncTaskLoaderActivity extends BaseActivity implements LoaderManag
 
     @Override
     public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        binding.progressBar.setVisibility(View.GONE);
         try {
-            // Convert the response into a JSON object.
             JSONObject jsonObject = new JSONObject(data);
-            // Get the JSONArray of book items.
             JSONArray itemsArray = jsonObject.getJSONArray("items");
 
-            // Initialize iterator and results fields.
             int i = 0;
             String title = null;
             String authors = null;
 
-            // Look for results in the items array, exiting when both the
-            // title and author are found or when all items have been checked.
+
             while (i < itemsArray.length() &&
                     (authors == null && title == null)) {
                 // Get the current item information.
                 JSONObject book = itemsArray.getJSONObject(i);
                 JSONObject volumeInfo = book.getJSONObject("volumeInfo");
 
-                // Try to get the author and title from the current item,
-                // catch if either field is empty and move on.
                 try {
                     title = volumeInfo.getString("title");
                     authors = volumeInfo.getString("authors");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-                // Move to the next item.
                 i++;
             }
 
-            // If both are found, display the result.
+
             if (title != null && authors != null) {
                 binding.titleText.setText(title);
                 binding.authorText.setText(authors);
-                //mBookInput.setText("");
             } else {
-                // If none are found, update the UI to show failed results.
                 binding.titleText.setText(R.string.no_results);
                 binding.authorText.setText("");
             }
 
         } catch (Exception e) {
-            // If onPostExecute does not receive a proper JSON string,
-            // update the UI to show failed results.
             binding.titleText.setText(R.string.no_results);
             binding.authorText.setText("");
             e.printStackTrace();
